f646e6c8f1bd30aec6599347a1806368c052cb2b
Nicolás Korzusehec <nkorzusehec@gmail.com>
2023-05-29 06:35:27 -0300

    reorganizo carpetas, sumo curso 4 en condiciones

--- Principiante Programacion/Mi primer página Web-AluraLATAM/Pruebas/Pruebas JavaScript/MuestrodeArrays.js
+++
@@ -1,9 +0,0 @@
-var Papitas=['Rústicas','Fritas','A la crema'];
-var Números=[1,2,3,4,5,6,7,8,9,10];
-
-function MuestreodeArrays(array) {
-    for (let Element of array) {console.log(Element)};
-};
-
-MuestreodeArrays (Números);
-MuestreodeArrays (Papitas);
--- Principiante Programacion/Mi primer página Web-AluraLATAM/Pruebas/Pruebas JavaScript/ReturnNumCombinatorio.js
+++
@@ -1,9 +0,0 @@
-function returncombinatorio(num) {
-    let total=1;
-        for(let i=num;i>1;i--)
-        {total=total*i;};
-    return total;
-    };
-console.log(returncombinatorio(4));
-console.log(returncombinatorio(8));
-console.log(returncombinatorio(3));
